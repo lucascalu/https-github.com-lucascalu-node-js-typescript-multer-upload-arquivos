763fb9f3d64270a86cfc137eb29f1493db49b868
lucascalu <54896910+lucascalu@users.noreply.github.com>
2023-09-26 14:38:31 -0300

    1 binaries uploaded

--- README.md
+++
@@ -1,3 +0,0 @@
-# https-github.com-lucascalu-node-js-typescript-multer-upload-arquivos
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-hz1muc)
--- index.js
+++ index.js
@@ -1,96 +1,37 @@
 const express = require('express');
 const multer = require('multer');
-const fs = require('file-system');
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectId;
-const myurl = 'mongodb://localhost:27017';
+const path = require('path');
 
-//CREATE EXPRESS APP
 const app = express();
-app.use(express.json());
+const port = 3000;
 
-// SET STORAGE
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads');
+// Configuração do Multer para salvar os arquivos no diretório 'uploads'
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads'); // O diretório onde os arquivos serão salvos
   },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now());
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    cb(null, Date.now() + ext); // Nome do arquivo no formato: timestamp.extensao
   },
 });
 
-var upload = multer({ storage: storage });
+const upload = multer({ storage });
 
-MongoClient.connect(myurl, (err, client) => {
-  if (err) return console.log(err);
-  db = client.db('test');
-  app.listen(3000, () => {
-    console.log('Database connected successfully');
-    console.log('Server started on port 3000');
-  });
-});
-//ROUTES WILL GO HERE
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
+// Rota para exibir um formulário HTML para fazer o upload de um arquivo
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-  const file = req.file;
-  if (!file) {
-    const error = new Error('Please upload a file');
-    error.httpStatusCode = 400;
-    return next(error);
+// Rota para lidar com o envio do arquivo
+app.post('/upload', upload.single('file'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).send('Nenhum arquivo foi enviado.');
   }
-  res.send(file);
+
+  res.send('Arquivo enviado com sucesso.');
 });
 
-//Uploading multiple files
-app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
-  const files = req.files;
-  if (!files) {
-    const error = new Error('Please choose files');
-    error.httpStatusCode = 400;
-    return next(error);
-  }
-  res.send(files);
-});
-
-app.post('/uploadphoto', upload.single('myImage'), (req, res) => {
-  var img = fs.readFileSync(req.file.path);
-  var encode_image = img.toString('base64');
-  // Define a JSONobject for the image attributes for saving to database
-
-  var finalImg = {
-    contentType: req.file.mimetype,
-    image: Buffer.from(encode_image, 'base64'),
-  };
-  db.collection('myCollection').insertOne(finalImg, (err, result) => {
-    console.log(result);
-    if (err) return console.log(err);
-    console.log('saved to database');
-    res.redirect('/');
-  });
-});
-
-app.get('/photos', (req, res) => {
-  db.collection('myCollection')
-    .find()
-    .toArray((err, result) => {
-      const imgArray = result.map((element) => element._id);
-      console.log(imgArray);
-      if (err) return console.log(err);
-      res.send(imgArray);
-    });
-});
-
-app.get('/photo/:id', (req, res) => {
-  var filename = req.params.id;
-  db.collection('myCollection').findOne(
-    { _id: ObjectId(filename) },
-    (err, result) => {
-      if (err) return console.log(err);
-      res.contentType('image/jpeg');
-      res.send(result.image.buffer);
-    }
-  );
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`);
 });
